47eaf002f4f10c7dbc1d608f718d9563307e96ed
anton2920 <anton2920@gmail.com>
2026-04-27 02:51:51 +0300

    Added tasks 6–8

--- training/DPM/hometasks/task7/make.bat
+++ training/DPM/hometasks/task7/make.bat
@@ -3,10 +3,11 @@
 set INST=C:\Intel
 set PATH=%PATH%;%INST%;%INST%\Sample
 
-PLM86 main.plm NOTYPE NOLIST OPTIMIZE(3)
+PLM86 main.plm TYPE DEBUG LIST CODE XREF OPTIMIZE(1)
+REM PLM86 main.plm DEBUG NOLIST OPTIMIZE(3)
 IF errorlevel = 1 GOTO STOP
 
-LINK86 main.obj, %INST%\Sample\doslibs.obj TO main.86 BIND
+LINK86 main.obj, %INST%\PLM86.LIB, %INST%\8087.LIB, %INST%\Sample\doslibs.obj TO main.86 BIND
 IF errorlevel = 1 GOTO STOP
 
 UDI2DOS main.86
